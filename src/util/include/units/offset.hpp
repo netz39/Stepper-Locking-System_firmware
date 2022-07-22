49347267467abbdf0677f11a5d32b0d77479738c
Michael Albrecht <micha.albrecht95@gmail.com>
2022-07-22 13:50:24 +0200

    Make use of =default constructor and {} for value initialisation

--- src/util/include/units/offset.hpp
+++ src/util/include/units/offset.hpp
@@ -9,20 +9,18 @@ template <typename SiUnit>
 class Offset
 {
 public:
-    constexpr Offset() : Offset(0.0f)
-    {
-    }
+    constexpr Offset() = default;
 
     constexpr explicit Offset(float offset) : offset{offset}
     {
     }
 
-    constexpr float getOffset() const
+    [[nodiscard]] constexpr float getOffset() const
     {
         return offset;
     }
 
 private:
-    const float offset;
+    const float offset{};
 };
 } // namespace units
