a4118f62d70b001e4fb581af00a795a2551e7693
Michael Albrecht <micha.albrecht95@gmail.com>
2022-07-22 13:54:03 +0200

    Fixed narrowing of LightDataSize in doAnimationStep()

--- src/LED/animations/RainbowAnimation.hpp
+++ src/LED/animations/RainbowAnimation.hpp
@@ -34,8 +34,8 @@ private:
 
     size_t rainbowIndex = 0;
 
-    static constexpr auto LightDataSize = 360;
-    static constexpr auto BrightnessFactor = 0.35;
+    static constexpr unsigned LightDataSize = 360;
+    static constexpr auto BrightnessFactor = 0.35f;
     static constexpr uint8_t SinTable[LightDataSize] = {
         0,   0,   0,   0,   0,   1,   1,   2,   2,   3,   4,   5,   6,   7,   8,   9,   11,  12,
         13,  15,  17,  18,  20,  22,  24,  26,  28,  30,  32,  35,  37,  39,  42,  44,  47,  49,
